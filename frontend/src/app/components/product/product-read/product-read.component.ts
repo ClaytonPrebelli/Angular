d3cc3c128ee2b89983f63a9aea6801db1209412e
Clayton Prebelli <clayton.pires@aluno.unip.br>
2021-01-24 18:52:47 -0300

    criado table de produtos

--- frontend/src/app/components/product/product-read/product-read.component.ts
+++ frontend/src/app/components/product/product-read/product-read.component.ts
@@ -10,6 +10,7 @@ import { Component, OnInit } from '@angular/core';
 export class ProductReadComponent implements OnInit {
 
     products!: Product[]
+    displayedColumns = ['id', 'name', 'price', 'action']
 
   constructor(private productService: ProductService) { }
 
@@ -19,5 +20,4 @@ export class ProductReadComponent implements OnInit {
      console.log(products)
    })
   }
-
 }
